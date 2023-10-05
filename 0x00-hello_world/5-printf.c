ce3f5c5bc869b54bf23151b313b29c844b73a88c
Peter Opoku-Mensah <mensahpeter421@gmail.com>
2023-10-05 10:27:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - this is the main function
- *
+ * main - this is a prinf function
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,/n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
