87a1d7cbc39cca5862b24243258680c1b1f8a41a
Peter Opoku-Mensah <mensahpeter421@gmail.com>
2023-10-06 10:37:42 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,3 +27,15 @@ int main(void)
 
 	else if (l_digit == 0)
 	{
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
+	}
+
+	else if (l_digit != 0 && l_digit < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		n, l_digit);
+	}
+
+
+	return (0);
+}
