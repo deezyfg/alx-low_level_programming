d2787c2271e5db5cdfb11a518b36c3cd8e015cca
Peter Opoku-Mensah <mensahpeter421@gmail.com>
2023-10-12 09:59:53 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print _numbers - Prints 0-9
+* @c: The character to check
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; i++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n')
+}
