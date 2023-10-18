e830204bc9548f7fc6986c4d22846c1bea09e345
Peter Opoku-Mensah <mensahpeter421@gmail.com>
2023-10-18 04:35:15 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @str: A pointer to the string.
+ *
+ * Return: *str
+ */
+
+char *rot13(char *str)
+{
+	int i;
+	int j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data1[j])
+				{
+				str[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
