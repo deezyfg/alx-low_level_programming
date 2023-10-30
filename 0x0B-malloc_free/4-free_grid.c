8bb5f40da6e150e19cabb9d3fece9173269c857d
Peter Opoku-Mensah <mensahpeter421@gmail.com>
2023-10-30 09:14:59 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * free_grid  - frees up a grid
+ * @grid: grid to check
+ * @height: height of grid
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
